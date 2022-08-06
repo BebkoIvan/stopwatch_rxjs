8cc68f827586dffd33d5e4323edcc839e2ac57c4
BebkoIvan <ivan.bebko@medondo.com>
2022-08-06 09:04:53 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { SecondsToMinutesPipe } from './seconds-to-minutes.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecondsToMinutesPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatCardModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
